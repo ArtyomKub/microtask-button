68e5161f7e72d60ee69c6cdbbed63f0e865de3a7
ArtyomKub <coder.kub.lover@gmail.com>
2023-06-05 20:35:09 +0200

    Start to create a universal button

--- src/App.tsx
+++ src/App.tsx
@@ -11,19 +11,26 @@ function App() {
     //     console.log('ALLA')
     // }
 
-    // const onClickHandler = (name: string) => {
-    //     console.log(name)
-    // }
+    const onClickHandler = (name: string) => {
+        console.log(name)
+    }
 
+    // const foo1 = () => {
+    //     console.log(100200)
+    // }
+    //
+    // const foo2 = (text: number) => {
+    //     console.log(text)
+    // }
 
     return (
         <div className="App">
             {/*<button onClick={(event)=>{console.log('Hello')}}>First</button>*/}
-            {/*<button onClick={(event)=>{onClickHandler('IVAN')}}>My YouTube Chanel - 1</button>*/}
-            {/*<button onClick={(event)=>{onClickHandler('ALLA')}}>My YouTube Chanel - 2</button>*/}
+            <button onClick={(event)=>{onClickHandler('IVAN')}}>My YouTube Chanel - 1</button>
+            <button onClick={(event)=>{onClickHandler('ALLA')}}>My YouTube Chanel - 2</button>
 
-            <button>1</button>
-            <button>2</button>
+            {/*<button onClick={foo1}>1</button>*/}
+            {/*<button onClick={(event)=>{foo2(100200)}}>2</button>*/}
 
         </div>
     );
