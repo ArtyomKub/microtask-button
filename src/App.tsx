0fc4a70f7a40bf2f513511da987838a0e32f3fb1
ArtyomKub <coder.kub.lover@gmail.com>
2023-06-05 20:03:02 +0200

    Create a new function: 'myFirstSubscriber' amd 'mySecondSubscriber'

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 import './App.css';
 
 function App() {
+    const myFirstSubscriber = () => {
+        console.log('IVAN')
+    }
+
+    const mySecondSubscriber = () => {
+        console.log('ALLA')
+    }
+
     return (
         <div className="App">
-            <button onClick={()=>{
-                console.log('HELLO')}}>My YouTube Chanel - 1</button>
+            <button onClick={myFirstSubscriber}>My YouTube Chanel - 1</button>
+            <button onClick={mySecondSubscriber}>My YouTube Chanel - 2</button>
         </div>
     );
 }
