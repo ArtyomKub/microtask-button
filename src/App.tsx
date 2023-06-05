a96584533a8340117a960c419d437e5ba061c173
ArtyomKub <coder.kub.lover@gmail.com>
2023-06-05 20:11:51 +0200

    Add event: React.MouseEvent<HtmlButtonElement>

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,17 @@ import React from 'react';
 import './App.css';
 
 function App() {
-    const myFirstSubscriber = () => {
+    const myFirstSubscriber = (event:React.MouseEvent<HTMLButtonElement>) => {
         console.log('IVAN')
     }
 
-    const mySecondSubscriber = () => {
+    const mySecondSubscriber = (event:React.MouseEvent<HTMLButtonElement>) => {
         console.log('ALLA')
     }
 
     return (
         <div className="App">
+            <button onClick={(event)=>{console.log('Hello')}}>First</button>
             <button onClick={myFirstSubscriber}>My YouTube Chanel - 1</button>
             <button onClick={mySecondSubscriber}>My YouTube Chanel - 2</button>
         </div>
