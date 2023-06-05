f845f86e854057342515dd6d8e451c986792235e
ArtyomKub <coder.kub.lover@gmail.com>
2023-06-05 20:21:36 +0200

    Create a function 'onClickHandler'

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import './App.css';
 
+
 function App() {
-    const myFirstSubscriber = (event:React.MouseEvent<HTMLButtonElement>) => {
-        console.log('IVAN')
+    // const myFirstSubscriber = (event:React.MouseEvent<HTMLButtonElement>) => {
+    //     console.log('IVAN')
+    // }
+    //
+    // const mySecondSubscriber = (event:React.MouseEvent<HTMLButtonElement>) => {
+    //     console.log('ALLA')
+    // }
+
+    const onClickHandler = (name: string) => {
+        console.log(name)
     }
 
-    const mySecondSubscriber = (event:React.MouseEvent<HTMLButtonElement>) => {
-        console.log('ALLA')
-    }
 
     return (
         <div className="App">
-            <button onClick={(event)=>{console.log('Hello')}}>First</button>
-            <button onClick={myFirstSubscriber}>My YouTube Chanel - 1</button>
-            <button onClick={mySecondSubscriber}>My YouTube Chanel - 2</button>
+            {/*<button onClick={(event)=>{console.log('Hello')}}>First</button>*/}
+            <button onClick={()=>{onClickHandler('IVAN')}}>My YouTube Chanel - 1</button>
+            <button onClick={()=>{onClickHandler('ALLA')}}>My YouTube Chanel - 2</button>
         </div>
     );
 }
