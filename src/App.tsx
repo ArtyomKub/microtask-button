4632c5ac54c5511dd00ef5ab247bf8d27b14f052
ArtyomKub <coder.kub.lover@gmail.com>
2023-06-05 19:54:15 +0200

    Add 'button'

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <button>My YouTube Chanel - 1</button>
+        </div>
+    );
 }
 
 export default App;
